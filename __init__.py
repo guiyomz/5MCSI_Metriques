286c1828e46654f588c7191988dad290d213a6d2
guiyomz <159912723+guiyomz@users.noreply.github.com>
2024-02-13 15:10:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,32 @@ def monhistogramme():
 @app.route('/')
 def hello_world():
     return render_template('hello.html') #Com
+
+@app.route('/commits/')
+def commitschart():
+    return rendertemplate('commitschart.html')
+
+@app.route('/api/commits/')
+def api_commits():
+    try:
+        url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
+        response = urlopen(url)
+        data = response.read().decode('utf-8')
+        commits_data = json.loads(data)
+        commits_by_minute = {}
+
+        for commit in commits_data:
+            commit_date = commit['commit']['author']['date']
+            date_object = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ')
+            minute = date_object.minute
+            if minute in commits_by_minute:
+                commits_by_minute[minute] += 1
+            else:
+                commits_by_minute[minute] = 1
+
+        return jsonify(commits_by_minute)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
   
 if __name__ == "__main__":
   app.run(debug=True)
