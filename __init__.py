370d7db76f69f73f3b4d87f3871b274a54ee4653
guiyomz <159912723+guiyomz@users.noreply.github.com>
2024-02-13 16:10:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,10 @@ def monhistogramme():
 def hello_world():
     return render_template('hello.html') #Com
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/commits/')
 def commitschart():
     return render_template('commits.html')
